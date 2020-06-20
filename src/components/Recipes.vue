fb19e81c18b7322b7d3f1a7c8b917e7f7f673980
Drew Kilz <drew@thespottedlynx.com>
2020-06-20 01:15:12 -0600

    Set up the filtering GUI (actual filtering not implemented).

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -12,6 +12,48 @@
                 sort-icon-left
                 :current-page="currentPage"
                 :per-page="perPage">
+            <template v-slot:thead-top>
+                <b-tr class="thead-light">
+                    <b-th>
+                        <b-input-group size="sm">
+                            <b-form-input :debounce="debounce" v-model="nameFilter" type="search" placeholder="Type to Search"></b-form-input>
+                        </b-input-group>
+                    </b-th>
+                    <b-th>
+                        <b-input-group size="sm">
+                            <b-form-input :debounce="debounce" v-model="minLevelFilter" type="number" placeholder="min" number min="1" max="95" step="1"></b-form-input>
+                        </b-input-group>
+                        <b-input-group size="sm">
+                            <b-form-input :debounce="debounce" v-model="maxLevelFilter" type="number" placeholder="max" number min="1" max="95" step="1"></b-form-input>
+                        </b-input-group>
+                    </b-th>
+                    <b-th>
+                        <b-input-group size="sm">
+                            <b-form-select v-model="classFilter" :options="classOptions" multiple select-size="2">
+                            </b-form-select>
+                        </b-input-group>
+                    </b-th>
+                    <b-th>
+                        <b-input-group size="sm">
+                            <b-form-select v-model="subclassFilter" :options="subclassOptions" multiple select-size="2">
+                            </b-form-select>
+                        </b-input-group>
+                    </b-th>
+                    <b-th>
+                        <b-input-group size="sm">
+                            <b-form-select v-model="skillFilter" :options="skillOptions" multiple select-size="2">
+                            </b-form-select>
+                        </b-input-group>
+                    </b-th>
+                    <b-th>
+                        <b-input-group size="sm">
+                            <b-form-select v-model="typeFilter" :options="typeOptions" multiple select-size="2">
+                            </b-form-select>
+                        </b-input-group>
+                    </b-th>
+                    <b-th></b-th>
+                </b-tr>
+            </template>
             <template v-slot:table-busy>
                 <div class="text-center">
                     <b-spinner small class="align-middle"></b-spinner> <strong>Loading...</strong>
@@ -31,6 +73,8 @@
 </template>
 
 <script>
+    import { compareValues } from '../utility.js'
+
     export default {
         name: 'Recipes',
         data() {
@@ -38,20 +82,28 @@
                 fields: [
                     {key: 'name', sortable: true, stickyColumn: true},
                     {key: 'level', sortable: true},
-                    {key: 'class', sortable: true, filterByFormatted: true,
-                        formatter: 'getClassName'},
-                    {key: 'subclass', sortable: true, filterByFormatted: true,
-                        formatter: 'getSubClassName', label:'Sub-Class'},
-                    {key: 'skill', sortable: true, filterByFormatted: true,
-                        formatter: 'getEnumValue'},
-                    {key: 'type', sortable: true, filterByFormatted: true,
-                        formatter: 'getEnumValue'},
+                    {key: 'class', sortable: true, formatter: 'getClassName'},
+                    {key: 'subclass', sortable: true, formatter: 'getSubClassName', label:'Sub-Class'},
+                    {key: 'skill', sortable: true, formatter: 'getEnumValue'},
+                    {key: 'type', sortable: true, formatter: 'getEnumValue'},
                     {key: 'add', label: ''}
                 ],
                 recipesLoading: false,
                 totalRows: 1,
                 currentPage: 1,
-                perPage: 5
+                perPage: 5,
+                nameFilter: '',
+                minLevelFilter: '',
+                maxLevelFilter: '',
+                classFilter: [],
+                subclassFilter: [],
+                skillFilter: [],
+                typeFilter: [],
+                classOptions: [],
+                subclassOptions: [],
+                skillOptions: [],
+                typeOptions: [],
+                debounce: 200
             }
         },
         methods: {
@@ -78,14 +130,34 @@
                     this.totalRows = response.data.count
                     return response.data.recipes || []
                 })
+            },
+            getCategoryOptions(categoryName) {
+                return this.axiosVnhApi.get(`categories/${categoryName}`)
+                    .then(response => {
+                        let options = []
+
+                        if (!response.data.types)
+                            return []
+
+                        for (let index in response.data.types) {
+                            let type = response.data.types[index]
+                            options.push({text: type.name, value: type.id})
+                        }
+
+                        options.sort(compareValues('text'))
+
+                        return options
+                    })
+                .catch(() => {
+                    return []
+                })
             }
         },
         mounted() {
-            this.axiosVnhApi.get('recipes/')
-                .then(response => {
-                    this.recipes = response.data.recipes
-                    this.recipesLoading = false
-                })
+            this.getCategoryOptions('Class').then(data => {this.classOptions = data})
+            this.getCategoryOptions('Sub Class').then(data => {this.subclassOptions = data})
+            this.getCategoryOptions('Skill').then(data => {this.skillOptions = data})
+            this.getCategoryOptions('Crafting Type').then(data => {this.typeOptions = data})
         }
     }
 </script>
